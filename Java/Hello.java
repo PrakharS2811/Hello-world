2b7603e6ff0b079599ef6c376b52566c8e2da6af
PrakharS2811 <42507333+PrakharS2811@users.noreply.github.com>
2018-10-16 13:21:24 +0530

    Update Hello.java

--- Java/Hello.java
+++ Java/Hello.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
 public class hello {
-
+    
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String myname=in.next();
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("Hello, World \n Grettings from "+myname);
     }
 
 }
